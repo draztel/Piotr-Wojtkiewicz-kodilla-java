6be9f23b8aa1e827bbad6a85fcb42f2fe53aadc1
draztel <poduszek.15@gmail.com>
2020-05-07 15:24:06 +0200

    problem z 6.3

--- src/test/java/com/kodilla/testing/forum/ForumTestSuite.java
+++ src/test/java/com/kodilla/testing/forum/ForumTestSuite.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class ForumTestSuite {
+    @Test
     public void testCaseUsername() {
 
         SimpleUser simpleuser = new SimpleUser("theForumUser");
