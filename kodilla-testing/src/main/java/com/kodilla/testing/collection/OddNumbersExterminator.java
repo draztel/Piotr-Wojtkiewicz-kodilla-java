9509cf46e041fd3d303711e8a2ddf5a0b5d6ca71
draztel <poduszek.15@gmail.com>
2020-05-07 19:33:46 +0200

    zadanie 6.3

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -0,0 +1,15 @@
+package com.kodilla.testing.collection;
+import java.util.*;
+
+public class OddNumbersExterminator {
+
+    ArrayList<Integer>numbers = new ArrayList<Integer>();
+
+    public void exterminate() {
+        for(int i = 0; i < numbers.size(); i++) {
+            if(numbers.get(i) % 2 == 1) {
+                numbers.remove(i);
+            }
+        }
+    }
+}
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -0,0 +1,46 @@
+package com.kodilla.testing.collection;
+
+import com.kodilla.testing.collection.OddNumbersExterminator;
+import org.junit.*;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class CollectionTestSuite {
+    @Before
+    public void before() {
+        System.out.println("Test Case: begin");
+    }
+    @After
+    public void after() {
+        System.out.println("Test Case: end");
+    }
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("Test Suite: begin");
+    }
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("Test Suite: end");
+    }
+    @Test
+    public void testOddNumbersExterminatorEmptyList() {
+        OddNumbersExterminator exterminator = new OddNumbersExterminator();
+
+        int result = exterminator.numbers.size();
+        System.out.println("Testing " + result);
+
+        Assert.assertEquals(0, result);
+    }
+    @Test
+    public void testOddNumbersExterminatorNormalList() {
+        OddNumbersExterminator exterminator = new OddNumbersExterminator();
+        exterminator.numbers.add(2);
+        exterminator.numbers.add(3);
+        exterminator.exterminate();
+
+        int result = exterminator.numbers.size();
+        System.out.println("Testing" + result);
+
+        Assert.assertEquals(1, result);
+    }
+}
