b2961f10218cddab06f2e1a354c8f1b573310ef5
draztel <poduszek.15@gmail.com>
2020-05-07 13:43:11 +0200

    modul 6.3

---
+++ src/test/java/com/kodilla/testing/forum/ForumTestSuite.java
@@ -0,0 +1,16 @@
+package com.kodilla.testing.forum;
+
+import com.kodilla.testing.user.SimpleUser;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ForumTestSuite {
+    public void testCaseUsername() {
+
+        SimpleUser simpleuser = new SimpleUser("theForumUser");
+
+        String result = simpleuser.getUsername();
+
+        Assert.assertEquals("theForumUser", result);
+    }
+}
