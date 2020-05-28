2ba362d745dce5be76d0267dfe3c1137b3c0f41e
draztel <poduszek.15@gmail.com>
2020-05-28 14:49:45 +0200

    Rock Paper Scissors game

--- kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
@@ -3,6 +3,7 @@ package com.kodilla.rps;
 import java.util.Scanner;
 
 public class RpsRunner {
+    char playerChoiceXOrN;
     int playerPoints = 0;
     int playerChoice;
     int computerPoints = 0;
@@ -18,13 +19,34 @@ public class RpsRunner {
         System.out.println("Please type your nickname to start a game");
         String playerNickname = scan.nextLine();
         while (!end) {
-            System.out.println("Please enter a number \n1 = ROCK\n2 = PAPER\n3 = SCISSORS\n0 = END GAME");
+            System.out.println("Please enter a number \n1 = ROCK\n2 = PAPER\n3 = SCISSORS");
             if (playerPoints == 3) {
                 System.out.println(playerNickname + " wins this game with the score of = " + playerPoints + " : " + computerPoints);
-                end = true;
+                System.out.println("Please press \nx = END GAME\nn = NEW GAME");
+                playerChoiceXOrN = scan.next().charAt(0);
+                if (playerChoiceXOrN == 'x') {
+                    System.out.println(playerNickname + " choosed to end game");
+                    end = true;
+                } else if (playerChoiceXOrN == 'n') {
+                    System.out.println(playerNickname + " choosed to start a new game");
+                    System.out.println("Please enter a number \n1 = ROCK\n2 = PAPER\n3 = SCISSORS");
+                    playerPoints = 0;
+                    computerPoints = 0;
+                }
+
             } else if (computerPoints == 3) {
                 System.out.println("Computer wins this game with the score of = " + computerPoints + " : " + playerPoints);
-                end = true;
+                System.out.println("Please press \nx = END GAME\nn = NEW GAME");
+                playerChoiceXOrN = scan.next().charAt(0);
+                if (playerChoiceXOrN == 'x') {
+                    System.out.println(playerNickname + " choosed to end game");
+                    end = true;
+                } else if (playerChoiceXOrN == 'n') {
+                    System.out.println(playerNickname + " choosed to start a new game");
+                    System.out.println("Please enter a number \n1 = ROCK\n2 = PAPER\n3 = SCISSORS");
+                    playerPoints = 0;
+                    computerPoints = 0;
+                }
             }
             playerChoice = scan.nextInt();
             computerChoice = (int) (Math.random() * (3 - 1 + 1) + 1);
@@ -40,30 +62,30 @@ public class RpsRunner {
                     System.out.println("Computer wins this round");
                     computerPoints++;
                     System.out.println("Computer points = " + computerPoints);
-                    System.out.println("Player points = " + playerPoints);
+                    System.out.println(playerNickname + " points = " + playerPoints);
                 } else if (computerChoice == 3) {
                     System.out.println(playerNickname + " = " + rock);
                     System.out.println("Computer = " + scissors);
-                    System.out.println("Player wins this round");
+                    System.out.println(playerNickname + " wins this round");
                     playerPoints++;
                     System.out.println("Computer points = " + computerPoints);
-                    System.out.println("Player points = " + playerPoints);
+                    System.out.println(playerNickname + " points = " + playerPoints);
                 }
             } else if (playerChoice == 2) {
                 if (computerChoice == 1) {
                     System.out.println(playerNickname + " = " + paper);
                     System.out.println("Computer = " + rock);
-                    System.out.println("Player wins this round");
+                    System.out.println(playerNickname + " wins this round");
                     playerPoints++;
                     System.out.println("Computer points = " + computerPoints);
-                    System.out.println("Player points = " + playerPoints);
+                    System.out.println(playerNickname + " points = " + playerPoints);
                 } else if (computerChoice == 3) {
                     System.out.println(playerNickname + " = " + paper);
                     System.out.println("Computer = " + scissors);
                     System.out.println("Computer wins this round");
                     computerPoints++;
                     System.out.println("Computer points = " + computerPoints);
-                    System.out.println("Player points = " + playerPoints);
+                    System.out.println(playerNickname + " points = " + playerPoints);
                 }
             } else if (playerChoice == 3) {
                 if (computerChoice == 1) {
@@ -72,18 +94,15 @@ public class RpsRunner {
                     System.out.println("Computer wins this round");
                     computerPoints++;
                     System.out.println("Computer points = " + computerPoints);
-                    System.out.println("Player points = " + playerPoints);
+                    System.out.println(playerNickname + " points = " + playerPoints);
                 } else if (computerChoice == 2) {
                     System.out.println(playerNickname + " = " + scissors);
                     System.out.println("Computer = " + paper);
-                    System.out.println("Player wins this round");
+                    System.out.println(playerNickname + " wins this round");
                     playerPoints++;
                     System.out.println("Computer points = " + computerPoints);
-                    System.out.println("Player points = " + playerPoints);
+                    System.out.println(playerNickname + " points = " + playerPoints);
                 }
-            } else if (playerChoice == 0) {
-                System.out.println("You choosed to end game");
-                end = true;
             }
         }
     }
@@ -93,7 +112,7 @@ public class RpsRunner {
         try {
             rpsRunner.rpsgame();
         } catch (Exception e) {
-            System.out.println("Please type a correct number");
+            System.out.println("You tried to enter incorrect value.");
         } finally {
             System.out.println("Try again!");
         }
