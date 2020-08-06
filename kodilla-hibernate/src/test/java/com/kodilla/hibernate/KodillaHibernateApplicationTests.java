d6c7a6dcb0f50e23c7f0cf6c598dfa5c447e982e
draztel <poduszek.15@gmail.com>
2020-08-06 11:30:42 +0200

    17.1

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/KodillaHibernateApplicationTests.java
@@ -0,0 +1,15 @@
+package com.kodilla.hibernate;
+
+import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KodillaHibernateApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
